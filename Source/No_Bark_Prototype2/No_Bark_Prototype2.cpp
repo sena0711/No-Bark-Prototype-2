0f3e109db86ac83ff4d558cb140e407caec498d0
Sena <love-diving@hotmail.com>
2017-03-09 17:55:33 +1300

    New project prototype 2

---
+++ Source/No_Bark_Prototype2/No_Bark_Prototype2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "No_Bark_Prototype2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, No_Bark_Prototype2, "No_Bark_Prototype2" );
---
+++ Source/No_Bark_Prototype2/No_Bark_Prototype2GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "No_Bark_Prototype2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NO_BARK_PROTOTYPE2_API ANo_Bark_Prototype2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
